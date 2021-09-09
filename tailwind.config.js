cadffa161c7a2d932844b5aa050d1693ac1ac9cc
bmikolajczak <br.mikolajczak@gmail.com>
2021-09-10 01:44:40 +0200

    whole project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        'custom-dark': '#06463B',
+        'lt': '#24787E'
+    },
+    fontFamily:{
+      'luxend': ['Lexend Deca',],
+      'shoulders': ['Big Shoulders Display',]
+    }
+  },
+    
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
